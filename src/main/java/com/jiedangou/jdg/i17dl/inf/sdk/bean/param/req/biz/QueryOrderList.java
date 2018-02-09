e87c21bc5f30d75333daab4d3c0f52d99f49d665
Howe Chiang <howechiang@gmail.com>
2018-02-09 17:38:37 +0800

    15000

--- src/main/java/com/jiedangou/jdg/i17dl/inf/sdk/bean/param/req/biz/QueryOrderList.java
+++ src/main/java/com/jiedangou/jdg/i17dl/inf/sdk/bean/param/req/biz/QueryOrderList.java
@@ -1,6 +1,9 @@
 package com.jiedangou.jdg.i17dl.inf.sdk.bean.param.req.biz;
 
-import org.nutz.lang.Lang;
+import com.jiedangou.jdg.i17dl.inf.sdk.bean.dict.Dict;
+import org.nutz.mvc.annotation.Param;
+
+import java.util.Date;
 
 /**
  * Created by Jianghao on 2017/12/22
@@ -9,30 +12,117 @@ import org.nutz.lang.Lang;
  */
 public class QueryOrderList {
 
-    private Integer pageNumber;
+    /**
+     * 每页数据量
+     */
     private Integer pageSize;
 
-    public Integer getPageNumber() {
-        return Lang.isEmpty(pageNumber) ? 1 : pageNumber;
-    }
-
-    public void setPageNumber(Integer pageNumber) {
-        this.pageNumber = pageNumber;
-    }
-
     public Integer getPageSize() {
-        return Lang.isEmpty(pageSize) ? 30 : pageSize;
+        return pageSize;
     }
 
     public void setPageSize(Integer pageSize) {
         this.pageSize = pageSize;
     }
 
-    public QueryOrderList() {
+    /**
+     * 页码
+     */
+    private Integer pageNumber;
+
+    public Integer getPageNumber() {
+        return pageNumber;
     }
 
-    public QueryOrderList(Integer pageNumber, Integer pageSize) {
+    public void setPageNumber(Integer pageNumber) {
         this.pageNumber = pageNumber;
+    }
+
+    /**
+     * 游戏ID
+     */
+    private Integer gameId;
+
+    public Integer getGameId() {
+        return gameId;
+    }
+
+    public void setGameId(Integer gameId) {
+        this.gameId = gameId;
+    }
+
+    /**
+     * 订单状态
+     * 0 已取消
+     * 1 已下单（未接手）
+     * 2 已接单（未处理）
+     * 3 处理中（代练中）
+     * 4 待验收
+     * 5 撤销中
+     * 6 仲裁中
+     * 7 异常
+     * 8 锁定
+     * 9 已下架
+     * 10 已撤销
+     * 11 已仲裁
+     * 12 强制仲裁
+     * 13 申述中（服务商申述）
+     * 14 已完成
+     * 88 已结算
+     * 99 已完结
+     */
+    private Integer orderStatus;
+
+    public Integer getOrderStatus() {
+        return orderStatus;
+    }
+
+    public void setOrderStatus(Integer orderStatus) {
+        this.orderStatus = orderStatus;
+    }
+
+    /**
+     * 查询开始时间
+     */
+    @Param(value = "queryStartTime", dfmt = Dict.DATE_FORMART_FULL)
+    private Date queryStartTime;
+
+    public Date getQueryStartTime() {
+        return queryStartTime;
+    }
+
+    public void setQueryStartTime(Date queryStartTime) {
+        this.queryStartTime = queryStartTime;
+    }
+
+    /**
+     * 查询开始时间
+     */
+    @Param(value = "queryEndTime", dfmt = Dict.DATE_FORMART_FULL)
+    private Date queryEndTime;
+
+    public Date getQueryEndTime() {
+        return queryEndTime;
+    }
+
+    public void setQueryEndTime(Date queryEndTime) {
+        this.queryEndTime = queryEndTime;
+    }
+
+    public QueryOrderList(Integer pageSize, Integer pageNumber) {
         this.pageSize = pageSize;
+        this.pageNumber = pageNumber;
+    }
+
+    public QueryOrderList(Integer pageSize, Integer pageNumber, Integer gameId, Integer orderStatus, Date queryStartTime, Date queryEndTime) {
+        this.pageSize = pageSize;
+        this.pageNumber = pageNumber;
+        this.gameId = gameId;
+        this.orderStatus = orderStatus;
+        this.queryStartTime = queryStartTime;
+        this.queryEndTime = queryEndTime;
+    }
+
+    public QueryOrderList() {
     }
 }
